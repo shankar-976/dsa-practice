b749ce4e5a5ca8c7641ddc33ebe566dd818bff56
shankar pachipala <shankarpachipala7@gmail.com>
2026-02-20 15:29:59 +0530

    completed the cses problem

--- cp31/cses/increasingarray.cpp
+++ cp31/cses/increasingarray.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n; cin>>n;
-    int a[n]; for(int i=0;i<n;i++) cin>>a[i];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int maxi = 0;
-    for(int i=0;i<n-1;i++){
-        if(a[i+1] < a[i]) maxi = max(a[i]-a[i+1]+1 , maxi);
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    long long moves = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[i - 1]) {
+            moves += (a[i - 1] - a[i]);
+            a[i] = a[i - 1];
+        }
     }
 
-    cout<<maxi;
-    
+    cout << moves << '\n';
+    return 0;
 }
